ecbe163e2ddd86c455220a958d652488feb18fc5
Amine <brahitiamine@hotmail.fr>
2024-10-14 22:47:11 +0200

    Probleme avec genere_equa

--- partie3.ml
+++ partie3.ml
@@ -8,10 +8,10 @@ let nouvelle_var () : string =
   compteur_var := !compteur_var + 1;
   "X" ^ (string_of_int !compteur_var)
 
-type ptype = Var of string | Arr of ptype * ptype | Nat
+type ptype = Varp of string | Arr of ptype * ptype | Nat
 let rec print_type (t : ptype) : string =
   match t with
-  Var x -> x
+  Varp x -> x
   | Arr (t1 , t2) -> "(" ^ ( print_type t1) ^" -> "^ ( print_type t2) ^")"
   |Nat -> ""
 
@@ -37,15 +37,15 @@ let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
       [(cherche_type v e, ty)]
 
   | App (t1, t2) -> 
-      let ta = Var (nouvelle_var_t ()) in
+      let ta = Varp (nouvelle_var_t ()) in
       let equa_t1 = genere_equa t1 (Arr (ta, ty)) e in
       let equa_t2 = genere_equa t2 ta e in
       equa_t1 @ equa_t2
 
   | Abs (x, t) -> 
-      let ta = Var (nouvelle_var_t ()) in
+      let ta = Varp (nouvelle_var_t ()) in
       let e' = (x, ta) :: e in
-      let tr = Var (nouvelle_var_t ()) in 
+      let tr = Varp (nouvelle_var_t ()) in 
       let equa_t = genere_equa t tr e' in
       let equa_abs = [(ty, Arr(ta, tr))] in
       equa_abs @ equa_t
@@ -53,12 +53,12 @@ let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
 let rec occur_check (s:string) (ty:ptype) : bool  = 
   match ty with
   | Arr (t1,t2) -> (occur_check s t1) || (occur_check s t2)
-  | Var x -> if x=s then true else false
+  | Varp x -> if x=s then true else false
   | _ -> false
 
 let rec substitution_type (s: string) (t_sub: ptype) (ty: ptype) : ptype =
   match ty with
-  | Var x -> if x = s then t_sub else Var x  (* Remplace la variable de type si c'est la bonne *)
+  | Varp x -> if x = s then t_sub else Varp x  (* Remplace la variable de type si c'est la bonne *)
   | Arr (t1, t2) -> Arr (substitution_type s t_sub t1, substitution_type s t_sub t2)  (* Applique la substitution aux sous-types *)
   | _ -> ty  (* Sinon, on laisse le type inchangé *)
   
@@ -66,3 +66,38 @@ let rec substitution_systeme (s: string) (t_sub: ptype) (systeme: (ptype * ptype
   List.map (fun (t1, t2) -> 
     (substitution_type s t_sub t1, substitution_type s t_sub t2)
   ) systeme
+
+
+
+let rec egalite_type (t1 : ptype) ( t2 :ptype) : bool = 
+  match t1,t2 with
+  |Varp s1, Varp s2 -> s1 = s2
+  |(Arr (t11,t12)),(Arr(t21,t22)) -> (egalite_type t11 t21) && (egalite_type t12  t22)
+  | Nat,Nat -> true 
+  | _,_ -> false
+ 
+let rec uni_step (eq : equa) : equa = 
+match eq with 
+| [] -> []  (* Si la liste d'équations est vide, on a fini *)
+| (t1, t2) :: queue -> 
+    if egalite_type t1 t2 
+    then uni_step queue  (* Si les types sont égaux, on supprime l'équation *)
+    else
+      match t1, t2 with
+      | Varp s, _ -> 
+          if occur_check s t2 
+          then failwith "Unification échoue : occur check"  (* Vérifier l'occurence *)
+          else 
+            (* Substitution de la variable s par t2 dans le reste des équations *)
+            let queue_substituee = List.map (fun (x, y) -> (substitution_type s t2 x, substitution_type s t2 y)) queue in
+            uni_step queue_substituee
+      | _, Varp s -> 
+          if occur_check s t1 
+          then failwith "Unification échoue : occur check" 
+          else 
+            let queue_substituee = List.map (fun (x, y) -> (substitution_type s t1 x, substitution_type s t1 y)) queue in
+            uni_step queue_substituee
+      | Arr (t1a, t1b), Arr (t2a, t2b) -> 
+          (* Ajouter deux nouvelles équations pour les sous-types *)
+          uni_step ((t1a, t2a) :: (t1b, t2b) :: queue)
+      | _ -> failwith "Unification échoue : types incompatibles"
